733fa79b66fde338d991aab35b5eb8bc51d81026
Andron00e <semenov.andrei.v@gmail.com>
2020-11-18 21:20:43 +0200

    Добавил solar_model_fixed.py и solar_intput_fixed.py

---
+++ solar_model_fixed.py
@@ -0,0 +1,55 @@
+# coding: utf-8
+# license: GPLv3
+
+gravitational_constant = 6.67408E-11
+"""Гравитационная постоянная Ньютона G"""
+
+
+def calculate_force(body, space_objects):
+    """Вычисляет силу, действующую на тело.
+
+    Параметры:
+
+    **body** — тело, для которого нужно вычислить дейстующую силу.
+    **space_objects** — список объектов, которые воздействуют на тело.
+    """
+
+    body.Fx = body.Fy = 0
+    for obj in space_objects:
+        if body == obj:
+            continue  # тело не действует гравитационной силой на само себя!
+        r = ((body.x - obj.x) ** 2 + (body.y - obj.y) ** 2) ** 0.5
+        body.Fx += gravitational_constant * body.m * obj.m / r ** 3 * (obj.x - body.x) #!!!
+        body.Fy += gravitational_constant * body.m * obj.m / r ** 3 * (obj.y - body.y) #!!!
+       
+
+
+def recalculate_space_objects_positions(space_objects, dt):
+    """Пересчитывает координаты объектов.
+
+    Параметры:
+
+    **space_objects** — список оьъектов, для которых нужно пересчитать координаты.
+    **dt** — шаг по времени
+    """
+    
+     #!!!
+    
+    for obj in space_objects:
+        obj.ax = obj.Fx / obj.m
+        obj.ay = obj.Fy / obj.m
+        obj.Vx += obj.ax * dt
+        obj.Vy += obj.ay * dt
+        obj.x += obj.Vx * dt
+        obj.y += obj.Vy * dt
+        
+    #!!!
+    
+    for body in space_objects:
+        calculate_force(body, space_objects)
+    for body in space_objects:
+        body.move(dt)
+
+
+if __name__ == "__main__":
+    print("This module is not for direct call!")
