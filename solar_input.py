0ab077c900df3509960a8c9679a345dd0e34ee56
Andron00e <semenov.andrei.v@gmail.com>
2020-11-20 01:25:09 +0200

    Переделал файл

--- solar_input.py
+++ solar_input.py
@@ -7,9 +7,7 @@ from solar_objects import CelestialBody
 def read_space_objects_data_from_file(input_filename):
     """Cчитывает данные о космических объектах из файла, создаёт сами объекты
     и вызывает создание их графических образов
-
     Параметры:
-
     **input_filename** — имя входного файла
     """
 
@@ -19,7 +17,7 @@ def read_space_objects_data_from_file(input_filename):
             if len(line.strip()) == 0 or line[0] == '#':
                 continue  # пустые строки и строки-комментарии пропускаем
             object_type = line.split()[0].lower()
-            if True or object_type == "star":  # FIXME: Добавь данные с файла
+            if object_type == "star":  # FIXME: Добавь данные с файла
                 star = CelestialBody(
                     "star",
                     1000,
@@ -35,6 +33,22 @@ def read_space_objects_data_from_file(input_filename):
                 )
                 parse_star_parameters(line, star)
                 objects.append(star)
+            elif object_type == "planet":
+                planet = CelestialBody(
+                    "planet"
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    5,
+                    "green",
+                    None
+                    )
+                parse_planet_parameters(line, planet)
+                objects.append(planet)
                 # FIXME: Делай тоже самое для планет
             else:
                 print("Unknown space object")
@@ -42,39 +56,25 @@ def read_space_objects_data_from_file(input_filename):
     return objects
 
 
-def parse_star_parameters(line, star):
-    """Считывает данные о звезде из строки.
+def parse_celestialbodies_parameters(line, object_type):
+    """Считывает данные о звезде/планете из строки.
     Входная строка должна иметь слеюущий формат:
-    Star <радиус в пикселах> <цвет> <масса> <x> <y> <Vx> <Vy>
-
-    Здесь (x, y) — координаты зведы, (Vx, Vy) — скорость.
+    Star/Planet <радиус в пикселах> <цвет> <масса> <x> <y> <Vx> <Vy>
+    Здесь (x, y) — координаты тела, (Vx, Vy) — скорость.
     Пример строки:
-    Star 10 red 1000 1 2 3 4
-
+    Star/Planet 10 red 1000 1 2 3 4
     Параметры:
-
     **line** — строка с описание звезды.
-    **star** — объект звезды.
+    **star/planet** — объект звезды.
     """
-
-    pass  # FIXME: not done yet
-
-
-def parse_planet_parameters(line, planet):
-    """Считывает данные о планете из строки.
-    Предполагается такая строка:
-    Входная строка должна иметь слеюущий формат:
-    Planet <радиус в пикселах> <цвет> <масса> <x> <y> <Vx> <Vy>
-
-    Здесь (x, y) — координаты планеты, (Vx, Vy) — скорость.
-    Пример строки:
-    Planet 10 red 1000 1 2 3 4
-
-    Параметры:
-
-    **line** — строка с описание планеты.
-    **planet** — объект планеты.
-    """
+    line = line.split()
+    object_type.R = float(line[1])
+    object_type.color = float(line[2])
+    object_type.m = float(line[3])
+    object_type.x = float(line[4])
+    object_type.y = float(line[5])
+    object_type.Vx = float(line[6])
+    object_type.Vy = float(line[7])
     pass  # FIXME: not done yet...
 
 
@@ -83,15 +83,22 @@ def write_space_objects_data_to_file(output_filename, space_objects):
     Строки должны иметь следующий формат:
     Star <радиус в пикселах> <цвет> <масса> <x> <y> <Vx> <Vy>
     Planet <радиус в пикселах> <цвет> <масса> <x> <y> <Vx> <Vy>
-
     Параметры:
-
     **output_filename** — имя входного файла
     **space_objects** — список объектов планет и звёзд
     """
     with open(output_filename, 'w') as out_file:
         for obj in space_objects:
-            print(out_file, "%s %d %s %f" % ('1', 2, '3', 4.5))
+            print(out_file, "%s %d %s %f" % ('type',  #!!!
+                                             obj.R,
+                                             obj.color,
+                                             obj.m, 
+                                             obj.x, 
+                                             obj.y, 
+                                             obj.Vx, 
+                                             obj.Vy
+                                             )
+                  )
             # FIXME: should store real values
 
 
--- solar_model.py
+++ solar_model.py
@@ -19,8 +19,9 @@ def calculate_force(body, space_objects):
         if body == obj:
             continue  # тело не действует гравитационной силой на само себя!
         r = ((body.x - obj.x) ** 2 + (body.y - obj.y) ** 2) ** 0.5
-        # body.Fx += 1  # FIXME: нужно вывести формулу...
-        # body.Fy += 2  # FIXME: нужно вывести формулу...
+        body.Fx += gravitational_constant * body.m * obj.m / r ** 3 * (obj.x - body.x)
+        body.Fy += gravitational_constant * body.m * obj.m / r ** 3 * (obj.y - body.y)
+       
 
 
 def recalculate_space_objects_positions(space_objects, dt):
@@ -32,6 +33,7 @@ def recalculate_space_objects_positions(space_objects, dt):
     **dt** — шаг по времени
     """
 
+    
     for body in space_objects:
         calculate_force(body, space_objects)
     for body in space_objects:
